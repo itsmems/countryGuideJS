4ccb8870d58aad400e17e7f87a66669767ae5f73
itsmems <manishshrestha0803@gmail.com>
2022-08-21 21:55:55 +0545

    complete all

--- app.js
+++ app.js
@@ -2,22 +2,67 @@ let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
 
 searchBtn.addEventListener("click", () => {
-  let countryName = "India";
+  let countryName = countryInp.value;
   let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
   console.log(finalURL);
   fetch(finalURL)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data[0]);
-      console.log(data[0].capital[0]);
-      console.log(data[0].flags.svg);
-      console.log(data[0].name.common);
-      console.log(data[0].continents[0]);
-      console.log(Object.keys(data[0].curriencies)[0]);
-      console.log(data[0].curriencies[Objects.keys(data[0].curriencies)].name);
-      console.log(
-        Object.values(data[0].languages).toString().split(",").join(",")
-      );
-      result.innerHTML = `<img src="${data[0].flags.svg}" class="flag-img">`;
+      //   console.log(data[0]);
+      //   console.log(data[0].capital[0]);
+      //   console.log(data[0].flags.svg);
+      //   console.log(data[0].name.common);
+      //   console.log(data[0].continents[0]);
+      //   console.log(Object.keys(data[0].curriencies)[0]);
+      //   console.log(data[0].currencies[Objects.keys(data[0].currencies)].name);
+
+      //   console.log(
+      //     Object.values(data[0].languages).toString().split(",").join(",")
+      //   );
+      result.innerHTML = `<img src="${data[0].flags.svg}" class="flag-img">
+      <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital: </h4>
+                <span>${data[0].capital[0]}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Continents: </h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Curriencies: </h4>
+                <span>${
+                  data[0].currencies[Object.keys(data[0].currencies)].name
+                } - ${Object.keys(data[0].currencies)[0]}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Common Language: </h4>
+                <span>${Object.values(data[0].languages)
+                  .toString()
+                  .split(",")
+                  .join(",")}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population: </h4>
+                <span>${data[0].population}</span>
+            </div>
+        </div>
+      `;
+    })
+    .catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `<h3> Empty</h3> `;
+      } else {
+        result.innerHTML = `<h3>Enter Valid Name</h3>`;
+      }
     });
 });
